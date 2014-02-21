3d9465f9560070153aceff421e3f582023baae63
Meris Bahtijaragic <meris.bahti@gmail.com>
2014-02-21 22:37:49 +0100

    initiate common model for server/client

---
+++ src/model/Request.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Request {
+
+  final int INSERT = 0;
+  final int DELETE = 1;
+  final int VIEW = 2;
+  final int ERROR = -1;
+
+}
---
+++ src/model/Response.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Response { 
+  
+  final int OK = 0;
+  final int DENIED_OR_EMPTY = 1;
+
+  final int ERROR = -1;
+
+
+}
