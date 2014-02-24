cdf6ec1a083422da4b0a42570206d108e2e0b546
Alexander <Alexander@alexmedkexASUS>
2014-02-24 17:47:19 +0100

    + Content for Journal. + getJournal() in AddRequest

--- src/model/Journal.java
+++ src/model/Journal.java
@@ -3,17 +3,33 @@ package model;
 
 public class Journal {
 
-  /**
-  public JSONObject toJSON() {
-      JSONObject obj = new JSONObject();
-      obj.put("id", "abc");
-      obj.put("content", "test");
-      return obj;
+	private int ID;
+	private String doctor;
+	private String nurse;
+	private String patient;
+	private String content;
+	private String district;
+	
+	class Journal(String doctor, String nurse, String patient, String content, String district){
+		this.doctor = doctor;
+		this.nurse = nurse;
+		this.patient = patient;
+		this.content = content;
+		this.district = district;
+	}
+  public String getContent() {
+    return content;
   }
-  **/
-
-  public String toString() {
-    return "hej";
+  
+  public String getDoctor(){
+	  return doctor;
+  }
+  
+  public String getNurse(){
+	  return nurse;
   }
 
+  public String getDistrict(){
+	  return district;
+  }
 }
