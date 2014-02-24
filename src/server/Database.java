16e407d0be35c2f13ed14fb7330310659e386eb8
Meris Bahtijaragic <meris.bahti@gmail.com>
2014-02-24 12:23:15 +0100

    fix database tests so that they don't have any output other than exceptions

--- src/server/Database.java
+++ src/server/Database.java
@@ -10,6 +10,9 @@ import java.sql.PreparedStatement;
 public class Database {
   Connection conn; 
 
+  /*
+   * Initiera klassen och leta efter SQLITE-JDBC drivern
+   */
   public Database() throws ClassNotFoundException {
     Class.forName("org.sqlite.JDBC");
     conn = null;
@@ -17,6 +20,9 @@ public class Database {
     init();
   }
 
+  /*
+   * Försök koppla dig till sqlite
+   */
   private void connect() {
     try {
       conn = DriverManager.getConnection("jdbc:sqlite:database.db");
@@ -25,6 +31,9 @@ public class Database {
     }
   }
 
+  /*
+   * ta bort gamla journaler och skapa tabeller för nya
+   */
   private void init() {
     try { 
       Statement statement = conn.createStatement();
@@ -36,6 +45,10 @@ public class Database {
     }
   }
 
+  /*
+   * lägg in journal
+   * returnera false om den redan finns eftersom ID är unikt?
+   */
   public void insertJournal(String id, String content) {
     try { 
       PreparedStatement pstatement = conn.prepareStatement("insert into journal values(?, ?)");
@@ -43,12 +56,16 @@ public class Database {
       pstatement.setString(2,content);
       pstatement.setQueryTimeout(30);  
       pstatement.executeUpdate();
-      /* @TODO: Skicka upp exception istället för att hantera här */
     } catch (SQLException e) {
       System.err.println("SQL Exception: " + e.getMessage());
     }
   }
 
+
+  /*
+   * hämta journal, borde returnera Journal eller typ...
+   * NullJournal / null t.ex om den inte har behörighet eller om journalen inte finns
+   */
   public void getJournal(String id) {
     try { 
       PreparedStatement pstatement = conn.prepareStatement("select * from journal where id=? ");
@@ -57,18 +74,27 @@ public class Database {
       pstatement.executeQuery();
       ResultSet rs = pstatement.executeQuery();
       // @TODO: Please hantera vad som sker om den är tom.
+      // @TODO: Please gör så att den returnerar en Journal
       while(rs.next())
       {
+        /*
         System.out.println("id = " + rs.getString("id"));
         System.out.println("content = " + rs.getString("content"));
+        */
       }
     } catch (SQLException e) {
       System.err.println("SQL Exception: " + e.getMessage());
     }
   }
 
+  /*
+   * Returnera en boolean om den lyckades
+   */
   public void deleteJournal(String id) {}
 
+  /*
+   * Returnera en godtycklig implementation av List<Journal>
+   */
   public void getMyJournals(String sslFingerPrint) {}
 
 
--- src/tests/DatabaseTest.java
+++ src/tests/DatabaseTest.java
@@ -1,32 +1,40 @@
 package tests;
 import server.Database;
+import server.*;
+import model.*;
+import org.junit.*;
 
 public class DatabaseTest extends junit.framework.TestCase {
+  Database db = null;
 
-    public void testNothing() {
+    @Before
+    public void before() {
+        try {
+          db = new Database();
+        } catch (ClassNotFoundException e) {
+          System.err.print("You done good'd");
+        }
     }
-    
+
+    @Test
     public void testWillAlwaysFail() {
       /* Initera DB */
-      Database db = null;
-
       /* Initera DB del 2
        * här måste vi ha någon classnotfoundexception för att jdbc kan strula.
        * men för att jag kådat så bra så kommer de aldri hända xD*/
+      /* populate database */
       try {
         db = new Database();
       } catch (ClassNotFoundException e) {
-        System.err.print("You done good'd");
+        System.err.print("You done goof'd");
       }
-
-      /* populate database */
+      
       db.insertJournal("fittpenis", "juggalowhoress");
       db.insertJournal("fittpenis2", "juggalowhoress2");
-
       /* titta lite på database */
       db.getJournal("fittpenis");
       db.getJournal("fittpenis2");
-      fail("An error message");
+      assertTrue(true);
     }
-    
+        
 }
