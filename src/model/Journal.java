275beebe4469102ac3aece4b6041d58a2c0c4066
Meris Bahtijaragic <meris.bahti@gmail.com>
2014-02-24 16:13:11 +0100

    remove json shit

--- src/model/Journal.java
+++ src/model/Journal.java
@@ -3,15 +3,6 @@ package model;
 
 public class Journal {
 
-  /**
-  public JSONObject toJSON() {
-      JSONObject obj = new JSONObject();
-      obj.put("id", "abc");
-      obj.put("content", "test");
-      return obj;
-  }
-  **/
-
   public String toString() {
     return "ta json datan och gör om den till strängdata";
   }
