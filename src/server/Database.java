86d51a742c9fbf89f8a84e9d91bb914e9b684f78
hannabjorgvinsdottir <hannabe@live.com>
2014-02-24 20:56:40 +0100

    Implemented a bunch of methods in Database.java, and added corresponding tests in DatabaseTest.java.

--- src/server/Database.java
+++ src/server/Database.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.sql.Connection;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -36,10 +38,13 @@ public class Database {
    */
   private void init() {
     try { 
+    	//Statement statement = conn.createStatement();
+    	//String sql = "drop table journal";
+    	
       Statement statement = conn.createStatement();
-      statement.setQueryTimeout(30);  
-      statement.executeUpdate("drop table if exists journal");
-      statement.executeUpdate("create table journal(id string, content string)");
+      statement.setQueryTimeout(30); 
+      //statement.executeUpdate(sql);
+      statement.executeUpdate("create table if not exists journal(ID INTEGER PRIMARY KEY   AUTOINCREMENT, doctor string, nurse string, patient string, district string, content string)");
     } catch (SQLException e) {
       System.err.println("SQL Exception: " + e.getMessage());
     }
@@ -49,11 +54,14 @@ public class Database {
    * lägg in journal
    * returnera false om den redan finns eftersom ID är unikt?
    */
-  public void insertJournal(String id, String content) {
+  public void insertJournal(String doctor, String nurse, String patient, String district, String content) {
     try { 
-      PreparedStatement pstatement = conn.prepareStatement("insert into journal values(?, ?)");
-      pstatement.setString(1,id);
-      pstatement.setString(2,content);
+      PreparedStatement pstatement = conn.prepareStatement("insert into journal (doctor, nurse, patient, district, content) values(?, ?, ?, ?, ?)");
+      pstatement.setString(1, doctor);
+      pstatement.setString(2, nurse);
+      pstatement.setString(3, patient);
+      pstatement.setString(4, district);
+      pstatement.setString(5, content);
       pstatement.setQueryTimeout(30);  
       pstatement.executeUpdate();
     } catch (SQLException e) {
@@ -66,10 +74,10 @@ public class Database {
    * hämta journal, borde returnera Journal eller typ...
    * NullJournal / null t.ex om den inte har behörighet eller om journalen inte finns
    */
-  public void getJournal(String id) {
+  public void getJournal(int id) {
     try { 
-      PreparedStatement pstatement = conn.prepareStatement("select * from journal where id=? ");
-      pstatement.setString(1,id);
+      PreparedStatement pstatement = conn.prepareStatement("select * from journal where ID=? ");
+      pstatement.setInt(1,id);
       pstatement.setQueryTimeout(30);  
       pstatement.executeQuery();
       ResultSet rs = pstatement.executeQuery();
@@ -77,10 +85,12 @@ public class Database {
       // @TODO: Please gör så att den returnerar en Journal
       while(rs.next())
       {
-        /*
-        System.out.println("id = " + rs.getString("id"));
+        System.out.println("id = " + rs.getInt("id"));
+        System.out.println("doctor = " + rs.getString("doctor"));
+        System.out.println("nurse = " + rs.getString("nurse"));
+        System.out.println("patient = " + rs.getString("patient"));
+        System.out.println("district = " + rs.getString("district"));
         System.out.println("content = " + rs.getString("content"));
-        */
       }
     } catch (SQLException e) {
       System.err.println("SQL Exception: " + e.getMessage());
@@ -90,12 +100,46 @@ public class Database {
   /*
    * Returnera en boolean om den lyckades
    */
-  public void deleteJournal(String id) {}
-
+  public boolean deleteJournal(int id) {
+	  try{
+		  PreparedStatement pstatement = conn.prepareStatement("delete from journal where ID = ?");
+		  pstatement.setInt(1, id);
+		  pstatement.executeUpdate();
+		  return true;
+	  }catch(SQLException e){
+		  System.err.println("SQL Exception: " + e.getMessage());
+		  return false;
+	  }
+  }
+  public boolean updateJournal(int id, String doctor, String nurse, String patient, String district, String content){
+	  try{
+		  PreparedStatement pstatement = conn.prepareStatement("select content from journal where id=?");
+		  pstatement.setInt(1, id);
+		  ResultSet rs = pstatement.executeQuery();
+		  String dbContent = "";
+		  while(rs.next()){
+			  dbContent = rs.getString("content");
+		  }
+		  String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+		  dbContent = String.format(dbContent + "%n %n" + date + "%n" + content);
+		  pstatement = conn.prepareStatement("update journal set doctor = ?, nurse = ?, patient = ?, district = ?, content = ? where ID = ?");
+		  pstatement.setString(1,doctor);
+		  pstatement.setString(2,nurse);
+		  pstatement.setString(3,patient);
+		  pstatement.setString(4,district);
+		  pstatement.setString(5,dbContent);
+		  pstatement.setInt(6,id);
+		  pstatement.executeUpdate();
+		  return true;
+	  }catch(SQLException e){
+		  System.err.println("SQL Exception: " + e.getMessage());
+		  return false;
+	  }
+  }
   /*
    * Returnera en godtycklig implementation av List<Journal>
    */
-  public void getMyJournals(String sslFingerPrint) {}
-
-
+  public void getMyJournals(String sslFingerPrint) {
+	  
+  }
 }
--- src/tests/DatabaseTest.java
+++ src/tests/DatabaseTest.java
@@ -19,15 +19,22 @@ public class DatabaseTest extends junit.framework.TestCase {
         }
     }
 
-    public void testWillAlwaysFail() {
+    public void testInsertJournal() {
       /* populate database */
-      
-      db.insertJournal("fittpenis", "juggalowhoress");
-      db.insertJournal("fittpenis2", "juggalowhoress2");
-      /* titta lite på database */
-      db.getJournal("fittpenis");
-      db.getJournal("fittpenis2");
+      db.insertJournal("Hanna", "Robin", "Meris", "Psyk","Massa info om patient");
+
       assertTrue(true);
     }
-        
+    public void testDeleteJournal() {
+    	//db.deleteJournal(1);
+    	db.getJournal(1);
+    }
+    public void testUpdate(){
+    	System.out.println("Journal before editing:");
+    	db.getJournal(2);
+    	boolean succeeded = db.updateJournal(2, "Hugo", "Berit", "Albin", "Right Wing", "Albin mar skit.");
+    	System.out.println("Journal after editing:");
+    	db.getJournal(2);
+    	assert(succeeded);
+    }
 }
