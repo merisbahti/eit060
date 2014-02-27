d610d5f2f357bc4d6ddff4df6a56f594e09cbbce
hannabjorgvinsdottir <hannabe@live.com>
2014-02-27 14:18:34 +0100

    Fix journal.javvava

--- src/model/Journal.java
+++ src/model/Journal.java
@@ -12,12 +12,21 @@ public class Journal implements Serializable{
 
 
 public Journal(String doctor, String nurse, String patient, String district, String content){
+	    ID = -1;
 		this.doctor = doctor;
 		this.nurse = nurse;
 		this.patient = patient;
 		this.content = content;
 		this.district = district;
 	}
+public Journal(int ID, String doctor, String nurse, String patient, String district, String content){
+	this.ID = ID;
+	this.doctor = doctor;
+	this.nurse = nurse;
+	this.patient = patient;
+	this.content = content;
+	this.district = district;
+}
   public String getContent() {
     return content;
   }
@@ -39,10 +48,8 @@ public Journal(String doctor, String nurse, String patient, String district, Str
 	
 	public String toString() {
 		return "Doc: " + doctor + "; Nurse: " + nurse + "; Patient: " + patient + "\n" + content;	
+	
 	}
-  public String getMetaData() {
-		return "Doc: " + doctor + "; Nurse: " + nurse + "; Patient: " + patient;	
-  }
 	public void setID(int ID){
 		this.ID = ID;
 	}
